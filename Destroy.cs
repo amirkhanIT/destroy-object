76a6d6de39d8cdbb0c1cee63f89bd462a930f5cb
amirkhanIT <127526619+amirkhanIT@users.noreply.github.com>
2023-03-10 21:17:25 +0600

    Destroy object
    you have to read in script

---
+++ Destroy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    /// you can write script if and put { }
+    ///  in  Destroy(gameObject);
+    void Start()
+    {
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
